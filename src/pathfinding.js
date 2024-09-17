fd86450fbca574613b332d424c683f531670b715
Daniel Greenheck <dgreenheck@gmail.com>
2024-09-17 13:03:35 -0500

    Implemented getNeighbors()

--- src/pathfinding.js
+++ src/pathfinding.js
@@ -2,12 +2,43 @@ import * as THREE from 'three';
 import { World } from './world';
 
 /**
- * 
+ * Finds the path between the start and end point (if one exists)
  * @param {THREE.Vector2} start 
  * @param {THREE.Vector2} end 
  * @param {World} world 
+ * @return {THREE.Vector2[]} Returns array of coordinates that make up the path
  */
 export function search(start, end, world) {
   const o = world.getObject(start);
-  console.log(o);
+
+  const neighbors = getNeighbors(start, world);
+  console.log(neighbors);
+}
+
+/**
+ * Returns array of coordinates for neighboring squares
+ * @param {THREE.Vector2} coords 
+ * @param {World} world
+ */
+function getNeighbors(coords, world) {
+  const neighbors = [];
+
+  // Left
+  if (coords.x > 0) {
+    neighbors.push(new THREE.Vector2(coords.x - 1, coords.y));
+  }
+  // Right
+  if (coords.x < world.width - 1) {
+    neighbors.push(new THREE.Vector2(coords.x + 1, coords.y));
+  }
+  // Top
+  if (coords.y > 0) {
+    neighbors.push(new THREE.Vector2(coords.x, coords.y - 1));
+  }
+  // Bottom
+  if (coords.y < world.height - 1) {
+    neighbors.push(new THREE.Vector2(coords.x, coords.y + 1));
+  }
+
+  return neighbors;
 }
